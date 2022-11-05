0eb9e0b40f70a61463b617ba29f19bd1d30dd1fc
bw55555 <brianwang2013@gmail.com>
2022-11-05 17:54:06 -0400

    add skills

---
+++ Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+    private int id;
+    private bool active;
+    private float cooldown;
+
+    public Skill(int id, float cooldown)
+    {
+        this.id = id;
+        active = false;
+        this.cooldown = cooldown;
+        this.currentCooldown = 0f;
+    }
+
+    public void decreaseCooldown()
+    {
+        this.currentCooldown -= 1f;
+    }
+
+    public void useSkill()
+    {
+        this.currentCooldown = cooldown;
+    }
+
+    public bool isReady()
+    {
+        return active && this.currentCooldown <= 0;
+    }
+
+    private float currentCooldown;
+
+    public int Id { get => id; set => id = value; }
+    public bool Active { get => active; set => active = value; }
+    public float Cooldown { get => cooldown; set => cooldown = value; }
+    public float CurrentCooldown { get => currentCooldown; set => currentCooldown = value; }
+}
