1fa1a843ab315f8fd005a9cb1bdfc9611c780ae0
tycelm <70135829+tycelm@users.noreply.github.com>
2022-11-05 15:17:48 -0400

    new enemy scripts

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -44,7 +44,7 @@ public class CharacterController2D : MonoBehaviour
 	private void FixedUpdate()
 	{
 		bool wasGrounded = m_Grounded;
-		m_Grounded = true;
+		m_Grounded = false;
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
