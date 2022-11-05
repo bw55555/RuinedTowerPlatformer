084659bf3766d7d8a8956dad80e85d15d93fdb37
tycelm <70135829+tycelm@users.noreply.github.com>
2022-11-05 18:41:56 -0400

    fixed pivot

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,18 +7,22 @@ public class PlayerAttack : MonoBehaviour
     public Transform hitbox;
     public float attackRange = 1f;
     public LayerMask enemyLayers;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("es");
             Attack();
         }
     }
 
     void Attack()
     {
+        animator.SetTrigger("Attack");
+
         Collider2D[] hit = Physics2D.OverlapCircleAll(hitbox.position, attackRange, enemyLayers);
 
         foreach (Collider2D enemy in hit)
