deca79993902cc6620593237b82119eaeba52c4e
tycelm <70135829+tycelm@users.noreply.github.com>
2022-11-06 18:34:29 -0500

    Free shop works B)

--- Assets/PickSkills.cs
+++ Assets/PickSkills.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class PickSkills : MonoBehaviour
 {
+    public GameObject origin;
     public Image first;
     public Image second;
     public Image third;
@@ -17,6 +18,7 @@ public class PickSkills : MonoBehaviour
     public Image one;
     public Image two;
     public Image three;
+    private int[] options = new int[3];
     void OnEnable()
     {
         Image[] icons = new Image[] { first, second, third, fourth, fifth, sixth, seventh, eighth };
@@ -25,28 +27,33 @@ public class PickSkills : MonoBehaviour
         while(count < 3)
         {
             selection = Random.Range(0, 8);
-            Debug.Log(selection);
-            Debug.Log(SkillContainer.Instance.getSkill((SkillType)selection).Active);
 
             if (!SkillContainer.Instance.getSkill((SkillType)selection).Active)
             {
                 if (count == 0)
                 {
-                    Debug.Log("got one");
-                    one.sprite = icons[selection].sprite;
+                    one.sprite = icons[selection - 1].sprite;
                 }
                 else if (count == 1)
                 {
-                    two.sprite = icons[selection].sprite;
+                    two.sprite = icons[selection - 1].sprite;
                 }
                 else
                 {
-                    three.sprite = icons[selection].sprite;
+                    three.sprite = icons[selection - 1].sprite;
                 }
+                options[count] = selection;
                 count += 1;
             }
         }
 
         
+    }
+
+    public void EquipSkill(int i)
+    {
+        SkillContainer.Instance.getSkill((SkillType)options[i]).Active = true;
+        origin.SetActive(false);
+        this.enabled = false;
     }
 }
