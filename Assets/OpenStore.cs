3494dc78ae5ee79a56a67a3d2acf3becff4ce62f
tycelm <70135829+tycelm@users.noreply.github.com>
2022-11-06 13:57:13 -0500

    Added the rooms

---
+++ Assets/OpenStore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenStore : MonoBehaviour
+{
+    public Image prompt;
+    public GameObject shop;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            prompt.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                shop.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            prompt.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                shop.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            prompt.enabled = false;
+        }
+    }
+}
