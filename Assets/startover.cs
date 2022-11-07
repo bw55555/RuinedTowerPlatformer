1d8f40a7a8f324a012b7c044f64240ff8058663b
Nealon Soltanpour <87830145+Fight-Fighter@users.noreply.github.com>
2022-11-07 09:44:49 -0500

    game over

---
+++ Assets/startover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start_Over()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+}
